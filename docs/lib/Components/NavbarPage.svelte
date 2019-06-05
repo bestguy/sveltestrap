0078700e35ccd23c827605d135d7cebb2750c297
Nathan Kerr <Nathan.Kerr@nordicsemi.no>
2019-06-05 08:25:18 -0700

    Navbar
    Collapse doesn't really work the same way as Reactstrap. Going to have to revisit it.

--- docs/lib/Components/NavbarPage.svelte
+++ docs/lib/Components/NavbarPage.svelte
@@ -1,1 +1,35 @@
-<div>Coming soon...</div>
+<script>
+	import PrismCode from '../UI/PrismCode.svelte';
+	import PageTitle from '../UI/PageTitle.svelte';
+	import SectionTitle from '../UI/SectionTitle.svelte';
+
+	import NavbarExample from '../examples/Navbar';
+	const NavbarExampleSource = require('!!raw-loader!../examples/Navbar').default;
+
+	import NavbarTogglerExample from '../examples/NavbarToggler';
+	const NavbarTogglerExampleSource = require('!!raw-loader!../examples/NavbarToggler').default;
+</script>
+
+<PageTitle title="Navbar" />
+<div class="docs-example">
+	<NavbarExample />
+</div>
+<pre>
+	<PrismCode class="language-html">
+		{NavbarExampleSource}
+	</PrismCode>
+</pre>
+
+<SectionTitle>NavbarToggler</SectionTitle>
+<p>
+	Place the <code>NavbarToggler</code> <b>after</b> <code>NavbarBrand</code> to have it appear on the right (typical setup).
+	Reverse the order to have it appear on the left
+</p>
+<div class="docs-example">
+	<NavbarTogglerExample />
+</div>
+<pre>
+	<PrismCode class="language-html">
+		{NavbarTogglerExampleSource}
+	</PrismCode>
+</pre>
---
+++ docs/lib/examples/Navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+	import {
+		Collapse,
+		Navbar,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavItem,
+		NavLink,
+		UncontrolledDropdown,
+		DropdownToggle,
+		DropdownMenu,
+		DropdownItem,
+	} from 'sveltestrap';
+
+	let isOpen = false;
+</script>
+
+<Navbar color="light" light expand="md">
+	<NavbarBrand href="/">sveltestrap</NavbarBrand>
+	<NavbarToggler on:click="{() => isOpen = !isOpen}" />
+	<Collapse {isOpen} navbar>
+		<Nav class="ml-auto" navbar>
+			<NavItem>
+				<NavLink href="#components/">Components</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+			</NavItem>
+			<UncontrolledDropdown nav inNavbar>
+				<DropdownToggle nav caret>
+					Options
+				</DropdownToggle>
+				<DropdownMenu right>
+					<DropdownItem>
+						Option 1
+					</DropdownItem>
+					<DropdownItem>
+						Option 2
+					</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>
+						Reset
+					</DropdownItem>
+				</DropdownMenu>
+			</UncontrolledDropdown>
+		</Nav>
+	</Collapse>
+</Navbar>
