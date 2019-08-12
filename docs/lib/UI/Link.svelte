af82502378200f0935534bc03b38f8567375ea43
bestguy <gary@gary.local>
2019-08-12 13:08:16 -0700

    Correct docs routing

---
+++ docs/lib/UI/Link.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Link } from 'svelte-routing';
+  let className = '';
+  export { className as class };
+  export let to = '';
+
+  function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+    return {
+      class: className
+    };
+  }
+</script>
+
+<Link to="{to}" getProps="{getProps}">
+  <slot />
+</Link>
