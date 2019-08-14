01dcfc35d6a045b258c594e6a2628eb599092114
bestguy <gary@gary.local>
2019-08-14 10:05:50 -0700

    Update Fade docs

---
+++ docs/lib/examples/Fade.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Button, CardBody, Card, Fade } from 'sveltestrap';
+	let isOpen = false;
+</script>
+
+<Button color="primary" on:click="{() => isOpen = !isOpen}" style="margin-bottom: 1rem;">Toggle</Button>
+<Fade {isOpen}>
+	<Card>
+		<CardBody>
+			Anim pariatur cliche reprehenderit,
+			enim eiusmod high life accusamus terry richardson ad squid. Nihil
+			anim keffiyeh helvetica, craft beer labore wes anderson cred
+			nesciunt sapiente ea proident.
+		</CardBody>
+	</Card>
+</Fade>
