0967235f54bb22ba6e4392877fe08837d386e834
Dayton <daytonlowell@me.com>
2019-12-31 22:59:04 -0600

    Added keyboard support to CarouselControl
    Defaults to true, you can pass keyboard={false} to turn it off

--- src/CarouselControl.svelte
+++ src/CarouselControl.svelte
@@ -1,5 +1,7 @@
 <script>
   import clsx from 'clsx';
+  import { onDestroy } from 'svelte';
+  import { clean, browserEvent } from './utils';
 
   let className = '';
   export { className as class };
@@ -8,42 +10,60 @@
   export let activeIndex = 0;
   export let items = [];
   export let wrap = true;
+  export let keyboard = true;
+  let _removeListener;
 
   $: classes = clsx(
     `carousel-control-${direction}`,
     className
   );
 
+  $: arrowKey = direction === 'prev' ? 'ArrowLeft' : 'ArrowRight';
+
   const getSrText = (direction) => {
     if(direction === 'next') {
-      return 'Next'
+      return 'Next';
     } else if(direction === 'prev') {
-      return 'Previous'
+      return 'Previous';
     }
   };
 
-  function changeActiveIndex(e) {
-    e.preventDefault()
+  onDestroy(() => {
+    if(_removeListener) {
+      _removeListener();
+    }
+  });
+
+  function changeActiveIndex(event) {
+    event.preventDefault();
     
     if(direction === 'prev') {
       if(activeIndex -1 < 0 && !wrap) {
-        return
+        return;
       }
 
-      activeIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1
+      activeIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
     } else if(direction === 'next') {
       if(activeIndex + 1 > items.length - 1 && !wrap) {
-        return
+        return;
       }
 
-      activeIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1
+      activeIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
     }
+  }
+
+  if(keyboard) {
+    _removeListener = browserEvent(document, 'keydown', (event) => {
+      if(event.key === arrowKey) {
+        changeActiveIndex(event);
+      }
+    })
   }
 
   $: srText = directionText ? directionText : getSrText(direction);
 </script>
 
-<a class="{classes}" role="button" href="#{direction}" on:click="{ e => changeActiveIndex(e) }">
+<a class="{classes}" role="button" href="#{direction}" on:click="{ e => changeActiveIndex(e, direction) }">
   <span class="carousel-control-{direction}-icon" aria-hidden="true"></span>
   <span class="sr-only">{srText}</span>
 </a>
--- src/CarouselItem.svelte
+++ src/CarouselItem.svelte
@@ -1,6 +1,5 @@
 <script>
   import clsx from 'clsx';
-  import { clean } from './utils';
   //import { fade, slide, fly, blur } from 'svelte/transition';
   //import { quintOut } from 'svelte/easing';
 
@@ -9,8 +8,6 @@
   export let itemIndex = 0;
   export let activeIndex = 0;
   export { className as class };
-
-  const props = clean($$props);
 
   $: classes = clsx(
     className,
