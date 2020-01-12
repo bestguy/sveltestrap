7a1fedef469633a50997947359cb17b1ae9c7a4d
bestguy <gary@gary.local>
2020-01-12 15:00:54 -0800

    Update stories

--- stories/carousel/WithCaptions.svelte
+++ stories/carousel/WithCaptions.svelte
@@ -1,44 +1,49 @@
 <script>
-  import { Carousel, CarouselControl, CarouselIndicators, CarouselItem, CarouselCaption } from "sveltestrap";
+  import {
+    Carousel,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselItem,
+    CarouselCaption
+  } from 'sveltestrap';
 
-  let items = [
-      {
-          url: 'https://i.ytimg.com/vi/FfsmLgD2ZFM/hqdefault.jpg',
-          title: 'goat 1',
-          subTitle: 'good goat'
-      },
-      {
-          url: 'https://i.ytimg.com/vi/rpufH_GchxI/hqdefault.jpg',
-          title: 'goat 2',
-          subTitle: 'better goat'
-      },
-      {
-          url: 'https://i.ytimg.com/vi/YxEzN8SLqkk/hqdefault.jpg',
-          title: 'goat 3',
-          subTitle: 'best goat'
-      },
+  const items = [
+    {
+      url:
+        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+      title: 'Slide 1',
+      subTitle: 'Slide 1'
+    },
+    {
+      url:
+        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+      title: 'Slide 2',
+      subTitle: 'Slide 2'
+    },
+    {
+      url:
+        'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+      title: 'Slide 3',
+      subTitle: 'Slide 3'
+    }
   ];
   let activeIndex = 0;
 </script>
 
-<Carousel
-  items="{items}"
-  bind:activeIndex="{activeIndex}"
-  ride="{ true }"
-  interval="{ 2000 }"
-  pause="{ true }"
->
-  <CarouselIndicators bind:activeIndex="{activeIndex}" items="{items}" />
-  
+<Carousel {items} bind:activeIndex ride={true} interval={2000}>
+  <CarouselIndicators bind:activeIndex {items} />
+
   <div class="carousel-inner">
     {#each items as item, index}
-    <CarouselItem bind:activeIndex="{activeIndex}" itemIndex="{index}">
-        <img src="{item.url}" class="d-block w-100" alt="{item.title}">
-        <CarouselCaption captionHeader="{item.title}" captionText="{item.subTitle}" />
-    </CarouselItem>
+      <CarouselItem bind:activeIndex itemIndex={index}>
+        <img src={item.url} class="d-block w-100" alt={item.title} />
+        <CarouselCaption
+          captionHeader={item.title}
+          captionText={item.subTitle} />
+      </CarouselItem>
     {/each}
   </div>
 
-  <CarouselControl direction="prev" bind:activeIndex="{activeIndex}" items="{items}" />
-  <CarouselControl direction="next" bind:activeIndex="{activeIndex}" items="{items}" />
+  <CarouselControl direction="prev" bind:activeIndex {items} />
+  <CarouselControl direction="next" bind:activeIndex {items} />
 </Carousel>
