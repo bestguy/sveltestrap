12ad623be3da16fbdfb848598cd70bb2a3ed7a14
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-12-27 16:25:00 +0900

    :white_check_mark: Add test case for tooltip

---
+++ src/__test__/Tooltip.spec.js
@@ -0,0 +1,50 @@
+import Tooltip from '../Tooltip.svelte';
+import Button from '../Button.svelte';
+import { render, cleanup } from '@testing-library/svelte';
+
+const renderTooltip = (props) => {
+  const { container } = render(Tooltip, { props });
+  return container;
+};
+const renderButton = (props) => {
+  const { container } = render(Button, { props });
+  container.id = 'btn';
+  return container;
+};
+
+beforeEach(() => {
+  cleanup();
+  renderButton({ children: 'Hello' });
+});
+
+const TOOLTIP_POSTION_CLASS = {
+  top: 'bs-tooltip-top',
+  bottom: 'bs-tooltip-bottom',
+  left: 'bs-tooltip-left',
+  right: 'bs-tooltip-right'
+};
+
+describe('Tooltip', () => {
+  it('should render text and default placement(top)', () => {
+    const containerTooltip = renderTooltip({
+      children: 'Hello world!',
+      target: 'btn'
+    });
+    const tooltip = containerTooltip.querySelector('.tooltip');
+    const tooltipInner = containerTooltip.querySelector('.tooltip-inner');
+    expect(tooltip.className.includes(TOOLTIP_POSTION_CLASS.top)).toBeTruthy();
+    expect(tooltipInner.innerHTML).toBe('Hello world!');
+  });
+
+  it('should render text and left placement', () => {
+    const containerTooltip = renderTooltip({
+      children: 'Hello world!',
+      placement: 'left',
+      target: 'btn'
+    });
+    const tooltip = containerTooltip.querySelector('.tooltip');
+    const tooltipInner = containerTooltip.querySelector('.tooltip-inner');
+    expect(tooltip.className.includes(TOOLTIP_POSTION_CLASS.left)).toBeTruthy();
+    expect(tooltipInner.innerHTML).toBe('Hello world!');
+  });
+});
