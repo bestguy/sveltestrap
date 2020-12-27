7ca376155cc08198aedc1a460ec3a9e17c0d82db
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-12-27 15:03:45 +0900

    :lipstick: Add tooltip stories with 4 differenct position

--- stories/tooltip/Index.svelte
+++ stories/tooltip/Index.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
   import ToolTip from '../../src/Tooltip.svelte';
+  import { Button } from 'sveltestrap';
 </script>
 
 <h1>Tooltip</h1>
 
-<ToolTip />
+
+<Button id="btn-top">TOP</Button>
+<ToolTip target={"btn-top"} placement={"top"}/>
+
+<Button id="btn-bottom">BOTTOM</Button>
+<ToolTip target={"btn-bottom"} placement={"bottom"}/>
+
+<Button id="btn-left">LEFT</Button>
+<ToolTip target={"btn-left"} placement={"left"}/>
+
+<Button id="btn-right">RIGHT</Button>
+<ToolTip target={"btn-right"} placement={"right"}/>
+
