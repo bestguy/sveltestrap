7010364ed3f40adf0152e3680fc5e5179e445dc2
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-12-27 17:18:49 +0900

    :lipstick: Update story tooltip basic sample

--- stories/tooltip/BasicSample.svelte
+++ stories/tooltip/BasicSample.svelte
@@ -2,12 +2,10 @@
   import Tooltip from '../../src/Tooltip.svelte';
   import { Button } from 'sveltestrap';
 
-  const placements: string[] = ['top', 'right', 'left', 'bottom'];
+  const placements: string[] = ['left', 'top', 'bottom', 'right'];
 </script>
 
 {#each placements as placement}
-  <div>
     <Button id={`btn-${placement}`}>{placement}</Button>
     <Tooltip target={`btn-${placement}`} {placement}>{placement}</Tooltip>
-  </div>
 {/each}
