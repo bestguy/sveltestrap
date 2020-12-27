0c1c8d13daf29bdc0a6ea4fee29cf96aa3e61af3
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-12-27 15:04:05 +0900

    :sparkles: Implement tooltip with id

--- src/Tooltip.svelte
+++ src/Tooltip.svelte
@@ -1,17 +1,33 @@
 <script>
+  import { onMount } from 'svelte';
+  import { createPopper } from '@popperjs/core/lib/popper-lite'
   // import classnames from './utils';
   let className = '';
   // export { className as class };
   export let target = '';
+  export let placement = 'top';
 
-  const tooltip = document.querySelector('#tooltip');
-  // createPopper(target, tooltip, {
-  //   placement: 'top',
-  // })
+  const randomDigit = () => {
+    const min = Math.ceil(0);
+    const max = Math.floor(100000);
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  const tooltipId = `tooltip${randomDigit()}`;
+
+  onMount(() => {
+    const tooltip = document.querySelector(`#${tooltipId}`);
+    const targetEl = document.querySelector(`#${target}`);
+  
+    createPopper(targetEl, tooltip, {
+      placement,
+    })
+  });
 </script>
 
+<h1>targetId: {target}</h1>
 
-<div id="tooltip" class="tooltip fade bs-tooltip-top show" role="tooltip" x-placement="top">
-  <div class="arrow"></div>
+<div id={tooltipId} class={`tooltip fade bs-tooltip-${placement} show`} role="tooltip" x-placement={placement}>
+  <div class="arrow" data-popper-arrow></div>
   <div class="tooltip-inner">Hello</div>
 </div>
