3e583722084aa20638d8a7168e72f0b44ec91e63
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-12-27 14:05:09 +0900

    :construction: Work in progress tooltip

--- src/Tooltip.svelte
+++ src/Tooltip.svelte
@@ -1,5 +1,17 @@
 <script>
-  import Button from './Button.svelte';
+  // import classnames from './utils';
+  let className = '';
+  // export { className as class };
+  export let target = '';
+
+  const tooltip = document.querySelector('#tooltip');
+  // createPopper(target, tooltip, {
+  //   placement: 'top',
+  // })
 </script>
 
-<Button data-toggle="tooltip" data-placement="top" title="hello">Hello</Button>
+
+<div id="tooltip" class="tooltip fade bs-tooltip-top show" role="tooltip" x-placement="top">
+  <div class="arrow"></div>
+  <div class="tooltip-inner">Hello</div>
+</div>
