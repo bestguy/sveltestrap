20dd2bd54bdf7f9f81f2804359a0646d5778ffbc
bestguy <gary@gary.local>
2021-03-25 08:42:40 -0700

    Update Modal
    - Support rendering modal when not open (requires newer svelte version)
    - Add static Modal support
    - Make backdrop optional
    - Make fade optional
    - Adjust fade duration to match Bootstrap default

--- stories/modal/Index.svelte
+++ stories/modal/Index.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
   import Example from '../Example.svelte';
+  import Backdrop from './Backdrop.svelte';
+  import backdropSource from '!!raw-loader!./Backdrop.svelte';
+  import Fade from './Fade.svelte';
+  import fadeSource from '!!raw-loader!./Fade.svelte';
   import Sample from './Sample.svelte';
   import sampleSource from '!!raw-loader!./Sample.svelte';
   import Sizes from './Sizes.svelte';
   import sizesSource from '!!raw-loader!./Sizes.svelte';
+  import Static from './Static.svelte';
+  import staticSource from '!!raw-loader!./Static.svelte';
 </script>
 
 <h1>Modals</h1>
@@ -17,4 +23,16 @@
 
 <Example title="Sizes" source={sizesSource}>
   <Sizes />
+</Example>
+
+<Example title="Backdrop" source={backdropSource}>
+  <Backdrop />
+</Example>
+
+<Example title="Fade" source={fadeSource}>
+  <Fade />
+</Example>
+
+<Example title="Static" source={staticSource}>
+  <Static />
 </Example>
