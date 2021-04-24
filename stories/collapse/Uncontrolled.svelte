da408179cf1e38aa7510ffa84bc0e8d08ebc3611
bestguy <7zark7@gmail.com>
2021-04-24 15:41:51 -0700

    Rework UncontrolledCollapse

--- stories/collapse/Uncontrolled.svelte
+++ stories/collapse/Uncontrolled.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { UncontrolledCollapse, Button, Card } from 'sveltestrap';
+  import { Collapse, Button, Card } from 'sveltestrap';
 </script>
 
 <Button color="primary" id="toggler" class="mb-3">Toggle</Button>
-<UncontrolledCollapse toggler="#toggler">
+<Collapse toggler="#toggler">
   <Card body>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt magni,
     voluptas debitis similique porro a molestias consequuntur earum odio
     officiis natus, amet hic, iste sed dignissimos esse fuga! Minus, alias.
   </Card>
-</UncontrolledCollapse>
+</Collapse>
