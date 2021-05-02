ebd71c1471bf1b802ced8800091d1742515791ce
bestguy <7zark7@gmail.com>
2021-05-02 07:12:01 -0700

    Add header prop to Modal

---
+++ stories/modal/Shorthand.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import {
+    Button,
+    Modal
+  } from 'sveltestrap';
+  let open = false;
+  const toggle = () => (open = !open);
+</script>
+
+<div>
+  <Button color="danger" on:click={toggle}>Open Modal</Button>
+  <Modal body header="Modal title" isOpen={open} {toggle}>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.
+  </Modal>
+</div>
