e2e366e1ca19d3c7ae0721478d933a7130157a65
bestguy <7zark7@gmail.com>
2021-05-14 08:35:11 -0700

    Re-add Jumbotron
    Helps with backwards compatibility, uses equivalent utility classes

---
+++ src/Jumbotron.svelte
@@ -0,0 +1,12 @@
+<script>
+  import classnames from './utils';
+
+  let className = '';
+  export { className as class };
+
+  $: classes = classnames(className, 'p-5 mb-4 bg-light rounded-3');
+</script>
+
+<div class={classes}>
+  <slot />
+</div>
