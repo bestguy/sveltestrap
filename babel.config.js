2944adebb15cb2046146eb585adc39276575d5db
Daniel Nahuel Acuna <danielnahuelacuna98@gmail.com>
2021-09-13 19:57:14 -0300

    fix: use just one babel file

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,21 @@
-const testingConfig = require('./babel.test.config')
-
-module.exports = api => {
-  const isTest = api.env('test');
-
-  if(isTest) return testingConfig;
-
-  return {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-typescript'
-    ]
-  };
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript'
+  ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            "targets": {
+              "node": "current"
+            }
+          }
+        ],
+        '@babel/preset-typescript'
+      ]
+    }
+  }
 };
