e671d4b007edf0d82f9ed345a1ee521aa0f9f5d7
bestguy <7zark7@gmail.com>
2022-01-22 16:48:04 -0800

    Improve FormGroup
    Extend correct HTML element props
    Add floating label support

---
+++ stories/formgroup/FloatingLabels.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Badge, Form, FormGroup, Input, Label } from 'sveltestrap';
+</script>
+
+<Form>
+  <FormGroup floating label="Floating Label">
+    <Input placeholder="Enter a value" />
+  </FormGroup>
+
+  <FormGroup floating>
+    <Input placeholder="Enter a value" />
+    <div slot="label">
+      Floating Label Slot <Badge>3</Badge>
+    </div>
+  </FormGroup>
+
+</Form>
---
+++ stories/formgroup/Index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Example from '../Example.svelte';
+  import FloatingLabels from './FloatingLabels.svelte';
+  import floatingLabelsSource from '!!raw-loader!./FloatingLabels.svelte';
+</script>
+
+<h1>FormGroup</h1>
+<a
+  href="https://getbootstrap.com/docs/5.1/forms/floating-labels/"
+  target="_blank"
+>
+  Bootstrap Floating Labels
+</a>
+
+<Example title="Floating Labels" source={floatingLabelsSource}>
+  <FloatingLabels />
+</Example>
