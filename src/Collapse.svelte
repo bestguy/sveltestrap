7f260a8a8559fc8d2fe6a30edcbf7f7e3d62064f
JulioHD <30412984+JulioHD@users.noreply.github.com>
2022-03-04 00:06:15 +0100

    Fixed collapse animation behaviour in navbars
    The component was having a short animation when opening a page with a navbar using it.
    Also the transition between screen sizes was also animated (when using expand attribute).
    This also fixes the hardcoded screen sizes and the corresponding pixels.
    The component will no longer be removed and readded to the dom, when it's open state changes.

--- src/Collapse.svelte
+++ src/Collapse.svelte
@@ -15,7 +15,6 @@
   export let onEntered = () => dispatch('open');
   export let onExiting = () => dispatch('closing');
   export let onExited = () => dispatch('close');
-  export let expand = false;
   export let toggler = null;
 
   onMount(() => toggle(toggler, (e) => {
@@ -25,55 +24,29 @@
 
   $: classes = classnames(className, {
     'collapse-horizontal': horizontal,
-    'navbar-collapse': navbar
+    'navbar-collapse': navbar,
+    'd-none': !isOpen
   });
-
-  let windowWidth = 0;
-  let _wasMaximized = false;
-
-  // TODO wrong to hardcode these here - come from Bootstrap CSS only
-  const minWidth = {};
-  minWidth['xs'] = 0;
-  minWidth['sm'] = 576;
-  minWidth['md'] = 768;
-  minWidth['lg'] = 992;
-  minWidth['xl'] = 1200;
 
   function notify() {
     dispatch('update', isOpen);
   }
-
-  $: if (navbar && expand) {
-    if (windowWidth >= minWidth[expand] && !isOpen) {
-      isOpen = true;
-      _wasMaximized = true;
-      notify();
-    } else if (windowWidth < minWidth[expand] && _wasMaximized) {
-      isOpen = false;
-      _wasMaximized = false;
-      notify();
-    }
-  }
 </script>
 
-<svelte:window bind:innerWidth={windowWidth} />
-
-{#if isOpen}
-  <div
-    style={navbar ? undefined : 'overflow: hidden;'}
-    {...$$restProps}
-    class={classes}
-    in:collapseIn={{ horizontal }}
-    out:collapseOut|local={{ horizontal }}
-    on:introstart
-    on:introend
-    on:outrostart
-    on:outroend
-    on:introstart={onEntering}
-    on:introend={onEntered}
-    on:outrostart={onExiting}
-    on:outroend={onExited}
-  >
-    <slot />
-  </div>
-{/if}
+<div
+  style={navbar ? undefined : 'overflow: hidden;'}
+  {...$$restProps}
+  class={classes}
+  in:collapseIn={{ horizontal }}
+  out:collapseOut|local={{ horizontal }}
+  on:introstart
+  on:introend
+  on:outrostart
+  on:outroend
+  on:introstart={onEntering}
+  on:introend={onEntered}
+  on:outrostart={onExiting}
+  on:outroend={onExited}
+>
+  <slot />
+</div>
