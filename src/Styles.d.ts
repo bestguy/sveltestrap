23155a98c05fc9cff04a9592a7ff57948cf4691b
bestguy <7zark7@gmail.com>
2023-05-31 19:23:49 -0700

    Add theme support to Style

--- src/Styles.d.ts
+++ src/Styles.d.ts
@@ -2,6 +2,7 @@ import { SvelteComponentTyped } from 'svelte';
 
 export interface StylesProps {
   icons?: boolean;
+  theme?: 'light' | 'dark' | 'auto' | undefined;
 }
 
 export default class Styles extends SvelteComponentTyped<StylesProps, {}, {}> {}
