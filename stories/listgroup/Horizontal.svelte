c95318559c396be56e7d917b37d496cc881a44e7
bestguy <7zark7@gmail.com>
2023-06-12 17:16:40 -0700

    Add ListGroup horizontal

---
+++ stories/listgroup/Horizontal.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { ListGroup, ListGroupItem } from 'sveltestrap';
+</script>
+
+<ListGroup horizontal>
+  <ListGroupItem active>Active</ListGroupItem>
+  <ListGroupItem>Lorem</ListGroupItem>
+  <ListGroupItem>Ipsum</ListGroupItem>
+  <ListGroupItem>Dolor</ListGroupItem>
+  <ListGroupItem>Sit</ListGroupItem>
+  <ListGroupItem disabled>Amet</ListGroupItem>
+</ListGroup>
