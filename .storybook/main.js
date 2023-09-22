50b85a13db61646471addac8bf508e5936a58190
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-09-22 06:06:48 -0700

    chore: upgrade to storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,21 @@
+/** @type { import('@storybook/svelte-vite').StorybookConfig } */
 module.exports = {
-  stories: ['../stories/index.stories.ts']
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx|svelte)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  features: {
+    // TODO: migrate
+    // https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#storystorev7-enabled-by-default
+    storyStoreV7: false,
+  },
+  framework: {
+    name: "@storybook/svelte-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  }
 };
